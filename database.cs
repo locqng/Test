4a10287553e5e20b9d5bd166f142e03bf106869f
locqng <qloc.ng@gmail.com>
2021-08-21 22:10:39 +1000

    Fix all things

--- database.cs
+++ database.cs
@@ -7,7 +7,6 @@ public class Form1 : Form
 
     private Button button;
     private TextBox textBox1;
-    private Label label1;
     public void FormLayout()
     {
         this.DisplayGUI();
@@ -16,18 +15,8 @@ public class Form1 : Form
             this.Name = "Employee Application";
             this.Text = "Employee Application";
             this.Size = new Size(1600, 800);
-            this.StartPosition = FormStartPosition.CenterScreen;
-            label1 = new Label();
-            label1.Size = new Size(100,100);
-            label1.Text = "Employee ID";
-            label1.Location = new Point(
-                (this.Width - button.Width) / 3 ,
-                (this.Height - button.Height) / 3);
             textBox1 = new TextBox();
             textBox1.Size = new Size(100,100);
-            textBox1.Location = new Point(
-                (this.Width - button.Width) /2 ,
-                0);
             button = new Button();
             button.Name = "button";
             button.Text = "Click Me!";
@@ -39,7 +28,6 @@ public class Form1 : Form
 
             this.Controls.Add(button);
             this.Controls.Add(textBox1);
-            this.Controls.Add(label1);
         }
 
         private void MyButtonClick(object source, EventArgs e) {
